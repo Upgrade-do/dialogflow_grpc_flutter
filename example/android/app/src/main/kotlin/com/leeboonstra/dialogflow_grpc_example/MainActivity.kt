9c9dbc13a73b60c1a15d5971c85a77ddd2514858
Jose <jfrometa@gmal.com>
2021-12-27 13:03:09 -0400

    removes example from project

--- example/android/app/src/main/kotlin/com/leeboonstra/dialogflow_grpc_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.leeboonstra.dialogflow_grpc_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/android/app/src/main/kotlin/com/savelee/flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.leeboonstra.flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
